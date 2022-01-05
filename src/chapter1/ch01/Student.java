e6a232b7b1960508a2955dcbf65dd6a51fb5091a
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-01-05 22:13:56 +0900

    File과 public의 관계

---
+++ src/chapter1/ch01/Student.java
@@ -0,0 +1,15 @@
+package chapter1.ch01;
+
+class Other {}
+/*
+ 하나의 파일에 클래스가 여러 개 올수는 있으나,
+ 퍼블릭은 하나여야함
+ */
+
+public class Student {
+    int studentNumber;
+    String studentName;
+    int majorCode;
+    String majorName;
+    int grade;
+}
