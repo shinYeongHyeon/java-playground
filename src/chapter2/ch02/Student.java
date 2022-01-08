8c2b1ed4b0cbe26211553a1acfa84ead77bce148
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-01-08 14:36:12 +0900

    Method

--- src/chapter2/ch02/Student.java
+++ src/chapter2/ch02/Student.java
@@ -7,9 +7,19 @@ class Other {}
  */
 
 public class Student {
-    int studentNumber;
-    String studentName;
-    int majorCode;
-    String majorName;
-    int grade;
+    public int studentId;
+    public String studentName;
+    public String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentId + " 학번의 이름은" + studentName + "이고, 주소는 " + address + "입니다");
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String name) {
+        studentName = name;
+    }
 }
